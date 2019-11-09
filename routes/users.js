63594b1854111679d8c891c50c8f5a7d54a62096
v-kedsou <55401636+v-kedsou@users.noreply.github.com>
2019-11-08 19:27:18 -0600

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json([{user: 'Adam', job: 'Global Security'}, {user: 'Chris', job: 'Blesser of Images'}, {user: 'Keegan', job: 'App Service OSS Support '}]);
+  res.json([{user: 'Adam', job: 'Global Security'}, {user: 'Chris', job: 'Blesser of Images'}, {user: 'Test', job: 'From Slot Branch '}]);
 });
 
 module.exports = router;
