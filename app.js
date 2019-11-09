8c6186b18ae31fbe5772d5f77f7208f814057a23
v-kedsou <55401636+v-kedsou@users.noreply.github.com>
2019-11-08 19:04:51 -0600

    Update app.js
    Removed CORS header settings

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(function(req, res, next){
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //res.header("Access-Control-Allow-Origin", "*");
+  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
 
